ca6a7abda77aa6b0e16d24e5b1c294f964b88203
Herney <cicotics@gmail.com>
2026-05-26 15:06:08 -0400

    feat(errors): custom error classes for Phase 2 (credit, invoices, approval, quote, catalog)

--- lib/errors.ts
+++ lib/errors.ts
@@ -5,3 +5,49 @@ export class FeatureDisabledError extends Error {
     this.name = 'FeatureDisabledError'
   }
 }
+
+export class CreditExceededError extends Error {
+  code = 'CREDIT_EXCEEDED'
+  constructor(
+    message: string,
+    public readonly available: string
+  ) {
+    super(message)
+    this.name = 'CreditExceededError'
+  }
+}
+
+export class InvoicesOverdueError extends Error {
+  code = 'INVOICES_OVERDUE'
+  constructor(
+    message: string,
+    public readonly count: number
+  ) {
+    super(message)
+    this.name = 'InvoicesOverdueError'
+  }
+}
+
+export class ApprovalAlreadyDecidedError extends Error {
+  code = 'APPROVAL_ALREADY_DECIDED'
+  constructor(message: string) {
+    super(message)
+    this.name = 'ApprovalAlreadyDecidedError'
+  }
+}
+
+export class QuoteExpiredError extends Error {
+  code = 'QUOTE_EXPIRED'
+  constructor(message: string) {
+    super(message)
+    this.name = 'QuoteExpiredError'
+  }
+}
+
+export class CatalogAccessDeniedError extends Error {
+  code = 'CATALOG_ACCESS_DENIED'
+  constructor(message: string) {
+    super(message)
+    this.name = 'CatalogAccessDeniedError'
+  }
+}
